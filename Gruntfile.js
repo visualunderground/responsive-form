a19f69502c5569e721c7b13627c62a81bccc5a0c
visualunderground <tom@visualunderground.co.uk>
2014-09-27 14:45:21 +0100

    Added simple grunt file sass and watch

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed'
+				},
+  		files: {
+					'assets/css/app.css' : 'assets/css/src/app.scss'
+				}
+			}
+		},
+		watch: {
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass']
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.registerTask('default',['sass']);
+}
