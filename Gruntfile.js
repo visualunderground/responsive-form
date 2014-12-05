b24a6158a2469d365e3e1f6c5b410ed142163037
visualunderground <tom@visualunderground.co.uk>
2014-12-05 13:28:15 +0000

    wip

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,13 +22,6 @@ module.exports = function(grunt) {
             },
         },
 
-        // combine_mq: {
-        //     default_options: {
-        //         src: 'dist/assets/css/app.css',
-        //         dest: 'dist/assets/css/app.combined.css'
-        //     }
-        // },
-
         scsslint: {
             allFiles: [
                 'app/scss/**/*.scss',
@@ -86,26 +79,52 @@ module.exports = function(grunt) {
                 src: 'dist/assets/js/application.js',
                 dest: 'dist/assets/js/application.min.js'
             }
+        },
+
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: 's2777.gridserver.com',
+                    port: 21,
+                    authKey: 'responsive-form.visualunderground.co.uk'
+                },
+                src: 'dist',
+                dest: '/domains/responsive-form.visualunderground.co.uk/html',
+                exclusions: ['dist/**/.DS_Store', 'dist/**/Thumbs.db']
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'dist/index.min.html': 'dist/index.html',     // 'destination': 'source'
+                }
+            }
         }
+
     });
-    //grunt.loadNpmTasks('grunt-combine-mq');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-uglify'); 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
     grunt.loadNpmTasks('grunt-notify');
     grunt.loadNpmTasks('grunt-scss-lint');
-
-    //grunt.registerTask('combine', ['combine_mq']);
     
 
     grunt.registerTask('build:css', ['scsslint', 'sass:dist', 'autoprefixer:dist']);
     grunt.registerTask('build:js', ['concat', 'uglify']);
-    grunt.registerTask('build', ['build:css', 'build:js']);
+    grunt.registerTask('build', ['build:css', 'build:js', 'htmlmin', 'ftp']);
 
     grunt.registerTask('default', ['sass:dist', 'autoprefixer:dist']);
 
+    grunt.registerTask('ftp', ['ftp-deploy']);
 
     grunt
 }
